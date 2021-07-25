8171ac67efd04e6fe8a6e9237310ee78a9f62ae7
Jacob McSwain <jacob@mcswain.dev>
2021-07-24 23:47:53 -0500

    Decode DeviceState proto messages

--- src/main.py
+++ src/main.py
@@ -1,15 +1,41 @@
 import serial
 import time
 
+from google import protobuf
+from google.protobuf import json_format
+from cobs import cobs
+
+import state_pb2
+
+
 def main():
     try:
         ser = serial.Serial('COM7', 9600, timeout=0.1)
         while True:
-            serMessage = ser.readline()
-            print(serMessage)
+            data = []
+            serByte = ser.read()
+            if serByte == b'':
+                pass
+            while serByte != b'\x00' and serByte != b'':
+                data.append(serByte)
+                serByte = ser.read()
+            data = b''.join(data)
+            deviceState = state_pb2.DeviceState()
+            try:
+                print(cobs.decode(data))
+                if deviceState.ParseFromString(cobs.decode(data)):
+                    print(
+                        json_format.MessageToJson(
+                            deviceState, including_default_value_fields=True)
+                    )
+            except protobuf.message.DecodeError as err:
+                print(err)
+            except cobs.DecodeError as err:
+                print(err)
             time.sleep(0.5)
     except KeyboardInterrupt:
         pass
+
 
 if __name__ == "__main__":
     main()
